d11bc679d9189eb77a9e1b11560e5132b3dea11b
zgorzal <zgorzalek.piotr@gmail.com>
2021-03-13 09:48:17 +0100

    Add DbUtil class method

--- src/main/java/DbUtil.java
+++ src/main/java/DbUtil.java
@@ -1,2 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "coderslab";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
 }
