ff65608a3e82ff7c22a647a08f414449b7a53f03
zgorzal <zgorzalek.piotr@gmail.com>
2021-03-13 11:18:22 +0100

    Add hashPassword and create method

---
+++ src/main/java/pl/coderslab/entity/MainDao.java
@@ -0,0 +1,7 @@
+package pl.coderslab.entity;
+
+public class MainDao {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/pl/coderslab/entity/UserDao.java
+++ src/main/java/pl/coderslab/entity/UserDao.java
@@ -1,5 +1,10 @@
 package pl.coderslab.entity;
 
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.sql.*;
+
+
 public class UserDao {
 
     private static final String CREATE_USER_QUERY =
@@ -13,5 +18,28 @@ public class UserDao {
 
     private static final String DELETE_USER_QUERY =
             "DELETE FROM users WHERE id = ?";
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User create(User user) {
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
